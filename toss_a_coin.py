69e2018b1ef34d6e2c1a537b1bbab1efbd2042e8
Valeria Arias <ariasvaleria9891@gmail.com>
2023-10-01 13:54:47 -0500

    1.0

---
+++ toss_a_coin.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+toss_coin= random.randint(0,1)
+if toss_coin==1:
+  print("Heads")
+else:
+  print("Tails")
